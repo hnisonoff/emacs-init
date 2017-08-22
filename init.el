4605dd3f57a88a542aa4a0e68e4c1c9f4509a899
Hunter Nisonoff <hunternisonoff@104.247.44.170.lightower.net>
2017-08-22 11:23:53 -0400

    Initialized emacs init file repository

---
+++ init.el
@@ -0,0 +1,22 @@
+;My emacs customizations
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
