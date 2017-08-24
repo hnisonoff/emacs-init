fa325cbd37686ca91e3c140abb731405d2c99163
Hunter Nisonoff <hunternisonoff@104.247.44.170.lightower.net>
2017-08-24 08:22:23 -0400

    Added packages and changed a few configurations

--- init.el
+++ init.el
@@ -1,11 +1,10 @@
 ;My emacs customizations
-
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 
-
+;;; Code:
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
