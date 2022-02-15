b5e0b6da0bee74b44ab74dcfd0cad0e00e4c0a37
Hunter Nisonoff <hunternisonoff@gmail.com>
2022-02-15 12:01:22 -0800

    Updated inits

--- init.el
+++ init.el
@@ -11,19 +11,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(conda-anaconda-home "/Users/hunternisonoff/anaconda3")
- '(ein:output-area-inlined-images t)
- '(markdown-command "/usr/local/bin/multimarkdown")
- '(package-selected-packages
-   '(column-enforce-mode column-marker jupyter emacs-jupyter poly-R poly-markdown modus-vivendi-theme modus-operandi-theme eglot company-quickhelp company-try-hard company-web company-statistics ein multi-term ox-gfm latex-math-preview curl anki-connect anki-editor anki-mode lsp-julia julia-lsp magit pdf-tools latex-preview-pane zenburn-theme which-key use-package symon request-deferred org-bullets lsp-ui lsp-python-ms git-gutter flymd flycheck fireplace exec-path-from-shell ess elpy elfeed counsel conda company-lsp auctex anaconda-mode ag))
- '(pdf-tools-handle-upgrades nil)
- '(preview-transparent-color "white")
  '(show-paren-mode t)
- '(tool-bar-mode nil))
+ '(tool-bar-mode nil)
+ '(tramp-encoding-shell "/bin/bash"))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(preview-face ((t (:background "white"))))
  '(preview-reference-face ((t (:background "white")))))
